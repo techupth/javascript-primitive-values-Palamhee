8abae965bf9fe7219b3dbd6dd3da3cd7ee598592
Palamhee <palarat.pj@gmail.com>
2024-03-27 02:31:50 +1300

    hand in Primitive Values assignment

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let deductedPrice = totalPrice * 0.25;
+let netPrice = totalPrice - deductedPrice;
+console.log(deductedPrice, netPrice); //
